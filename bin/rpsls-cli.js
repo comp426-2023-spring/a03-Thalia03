1ce97dda17b5990579c65923b08a774a1db26c33
Thalia Rodriguez <thalia03@ad.unc.edu>
2023-05-09 22:15:16 -0400

    bytd

--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -3,33 +3,28 @@ import minimist from "minimist";
 import { rpsls } from "../lib/rpsls.js";
 
 const args = minimist(process.argv.slice(2));
+const h = `Usage: node-rps [SHOT]
+Play Rock Paper Scissors (RPS)
+  -h, --help      display this help message and exit
+  -r, --rules     display the rules and exit
+Examples:
+  node-rps        Return JSON with single player RPS result.
+                  e.g. {"player":"rock"}
+  node-rps rock   Return JSON with results for RPS played against a simulated opponent.
+                  e.g {"player":"rock","opponent":"scissors","result":"win"}`;
 
+const r = `Rules for Rock Paper Scissors:
+- Scissors CUTS Paper
+- Paper COVERS Rock
+- Rock CRUSHES Scissors
+`;
 
 if(args.h || args.help) {
-
-    console.log(`
-
-    Usage: node-rps [SHOT]
-    Play Rock Paper Scissors (RPS)
-      -h, --help      display this help message and exit
-      -r, --rules     display the rules and exit
-    Examples:
-      node-rps        Return JSON with single player RPS result.
-                      e.g. {"player":"rock"}
-      node-rps rock   Return JSON with results for RPS played against a simulated opponent.
-                      e.g {"player":"rock","opponent":"scissors","result":"win"}
-    `
-    ); process.exit(0);
+  console.log(h); process.exit(0);
 }
 
 if(args.r || args.rules) {
-    console.log( `
-    Rules for Rock Paper Scissors:
-      - Scissors CUTS Paper
-      - Paper COVERS Rock
-      - Rock CRUSHES Scissors
-    `)
-    process.exit(0);
+    console.log(r); process.exit(0);
 }
 
 let shot = args._[0];
@@ -38,23 +33,10 @@ try {
   let result = rpsls(shot);
   console.log(JSON.stringify(result));
 } catch (e) {
-  console.log(`
-
-  Usage: node-rps [SHOT]
-  Play Rock Paper Scissors (RPS)
-    -h, --help      display this help message and exit
-    -r, --rules     display the rules and exit
-  Examples:
-    node-rps        Return JSON with single player RPS result.
-                    e.g. {"player":"rock"}
-    node-rps rock   Return JSON with results for RPS played against a simulated opponent.
-                    e.g {"player":"rock","opponent":"scissors","result":"win"}
-  `);
-  console.log( `
-  Rules for Rock Paper Scissors:
-    - Scissors CUTS Paper
-    - Paper COVERS Rock
-    - Rock CRUSHES Scissors
-  `);
+  console.log(h);
+  console.log(r);
 }
+
+
+
 
